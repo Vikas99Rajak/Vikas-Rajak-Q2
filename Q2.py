6bfd88b98c03c5bd3ca6d7998eacdf54912907d2
Vikas99Rajak <84638696+Vikas99Rajak@users.noreply.github.com>
2021-05-23 15:56:42 +0530

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -14,7 +14,7 @@ def minChar(n, password):
 	print("Password contains\nSpecial Characters =", count[0], "\nDigits =", count[1], "\nUppercase Alphabets =", count[2], "\nLowercase Alphabets=", count[3]);
 	if(n >= 8):
 		if 0 not in count:
-			print("Strong password good to go!")
+			print("Strong password, good to go!")
 	else:
 		print(8-n, "more Characters required")
 
